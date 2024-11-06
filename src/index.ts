f8c8a59d73cffad76a5d655ab86055bd55e54d05
Bond <4683867+bondz@users.noreply.github.com>
2024-11-06 17:02:28 -0600

    Use node provider if config is missing

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
-import { fromIni } from '@aws-sdk/credential-providers';
+import {
+  fromIni,
+  fromNodeProviderChain,
+  createCredentialChain,
+} from '@aws-sdk/credential-providers';
+import { CredentialsProviderError } from '@smithy/property-provider';
 
 export function vertexProvider(init?: Parameters<typeof fromIni>[0]) {
-  return fromIni({
-    ...init,
-    logger: init?.logger,
-    configFilepath: init?.configFilepath ?? process.cwd() + '/aws.config',
-    profile: init?.profile ?? process.platform,
-  });
+  return createCredentialChain(
+    fromIni({
+      ...init,
+      logger: init?.logger,
+      configFilepath: init?.configFilepath ?? process.cwd() + '/aws.config',
+      profile: init?.profile ?? process.platform,
+    }),
+    async () => {
+      init?.logger?.debug('aws.config file not found');
+
+      throw new CredentialsProviderError(
+        'aws.config missing, using default credentials',
+        {
+          tryNextLink: true,
+          logger: init?.logger,
+        }
+      );
+    },
+    fromNodeProviderChain(init)
+  );
 }
